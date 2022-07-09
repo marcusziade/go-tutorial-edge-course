fea63d26d77d418a7df78428aeee39a603cbe6b3
Marcus Ziadé <marcus.ziade@ouraring.com>
2022-07-09 17:30:48 +0300

    Add constant

--- the-beginners-guide/main.go
+++ the-beginners-guide/main.go
@@ -6,8 +6,13 @@ func main() {
 	hello := "Hello Gopher"
 	fmt.Println(hello)
 
+	// Variable
 	myAge := 29
 	myAge++
 	myAge -= 15
 	println(myAge)
+
+	// Constant
+	const whatif = "What if?"
+	println(whatif)
 }
