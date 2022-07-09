ba4c33713b81cbb9c88d732ce6cbfce3b1764296
Marcus Ziadé <marcus.ziade@ouraring.com>
2022-07-09 18:17:36 +0300

    Add if statement

--- the-beginners-guide/main.go
+++ the-beginners-guide/main.go
@@ -3,16 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	hello := "Hello Gopher"
-	fmt.Println(hello)
+	// if statements
+	customerHeight := 130
+	customerAge := 29
 
-	// Variable
-	myAge := 29
-	myAge++
-	myAge -= 15
-	println(myAge)
-
-	// Constant
-	const whatif = "What if?"
-	println(whatif)
+	if customerHeight >= 150 && customerAge >= 18 {
+		fmt.Println("Can access")
+	} else if customerHeight >= 120 {
+		println("Can almost access")
+	} else {
+		fmt.Println("Can not access")
+	}
 }
