cf9c95b2f76a2d729bc65ea5371be6353e446741
Marcus Ziadé <marcus.ziade@ouraring.com>
2022-07-09 18:24:53 +0300

    Add switch statement

--- the-beginners-guide/main.go
+++ the-beginners-guide/main.go
@@ -1,17 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
-	// if statements
-	customerHeight := 130
-	customerAge := 29
+	// switch statements
+	customerHeight := 100
+	customerAge := 10
 
-	if customerHeight >= 150 && customerAge >= 18 {
-		fmt.Println("Can access")
-	} else if customerHeight >= 120 {
-		println("Can almost access")
-	} else {
-		fmt.Println("Can not access")
+	switch {
+	case customerHeight >= 130 || customerAge >= 18:
+		fmt.Println("Can access all rides")
+	case customerHeight >= 120:
+		fmt.Println("Can access children rides")
+	default:
+		fmt.Println("Can't access rides")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		println("macOS")
+	case "linux":
+		println("Linux machine")
+	default:
+		println("Something else")
 	}
 }
