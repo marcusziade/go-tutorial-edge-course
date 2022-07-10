b6192c4d559c11df11ad0a837ee77ba0e0dcdfc1
Marcus Ziadé <marcus.ziade@ouraring.com>
2022-07-10 08:52:18 +0300

    Add array

--- the-beginners-guide/main.go
+++ the-beginners-guide/main.go
@@ -6,11 +6,43 @@ import (
 )
 
 func main() {
-	createSwitch()
+	createArray()
+}
+
+func createArray() {
+	planets := []string{"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"}
+	planets = append(planets, "Pluto")
+	for _, planet := range planets {
+		fmt.Println(planet)
+	}
+}
+
+func createVariables() {
+	myAge := 29
+	myAge++
+	myAge -= 15
+	println(myAge)
+}
+
+func createIfStatement() {
+	customerHeight := 130
+	customerAge := 29
+
+	if customerHeight >= 150 && customerAge >= 18 {
+		fmt.Println("Can access")
+	} else if customerHeight >= 120 {
+		println("Can almost access")
+	} else {
+		fmt.Println("Can not access")
+	}
+}
+
+func createConstants() {
+	const whatif = "What if?"
+	println(whatif)
 }
 
 func createSwitch() {
-	// switch statements
 	customerHeight := 100
 	customerAge := 10
 
