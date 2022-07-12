10160ade8c47d46312e442c28727985129f332a0
Marcus Ziadé <marcus.ziade@ouraring.com>
2022-07-12 10:19:04 +0300

    Add network package

--- the-beginners-guide/practice-project/cmd/cli/main.go
+++ the-beginners-guide/practice-project/cmd/cli/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/marcusziade/go-tutorial-edge-course/network"
+)
+
 func main() {
 	println("Awesome CLI 0.0.1")
+
+	network.Ping("")
 }
---
+++ the-beginners-guide/practice-project/network/ping.go
@@ -0,0 +1,6 @@
+package network
+
+// Ping - Take in an IP and ping that IP before returning a response
+func Ping(ip string) {
+	println(ip)
+}
